345db99eef3031a75e1cd20a2fabaa0e1ec92710
wa1ful <plach4747@gmail.com>
2025-12-13 12:48:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot 
 from config import token
-
 from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
@@ -14,6 +13,34 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[username]
+        result = pokemon.feed()
+        bot.reply_to(message, result)
+    else:
+        bot.reply_to(message, "Сначала создай покемона командой /go")
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[username]
+        info_text = f"{pokemon.info()}\n\nУровень: {pokemon.level}\nОпыт: {pokemon.exp}/{pokemon.exp_to_next_level}\nСытость: {pokemon.hunger}%\nСчастье: {pokemon.happiness}%\nВес: {pokemon.weight:.1f}кг\nЕда: {pokemon.inventory.get('food', 0)}"
+        bot.send_message(message.chat.id, info_text)
+    else:
+        bot.reply_to(message, "Сначала создай покемона командой /go")
+
+@bot.message_handler(commands=['achievements'])
+def achievements(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[username]
+        achievements_text = pokemon.get_achievements()
+        bot.send_message(message.chat.id, achievements_text)
+    else:
+        bot.reply_to(message, "Сначала создай покемона командой /go")
 
 bot.infinity_polling(none_stop=True)
-
